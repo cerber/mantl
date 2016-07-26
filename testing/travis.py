9fe4e852b20766bab48bf1d0f9591a74879f6fbf
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-07-26 11:46:49 -0500

    [TRAVIS] Don't cache TF binaries, unzip elsewhere

--- testing/travis.py
+++ testing/travis.py
@@ -21,11 +21,13 @@ import terraform as tf
 ## TRAVIS STEPS
 
 def install():
+    os.environ['PATH'] += ":~/bin"
     run_cmds([
         (["pip", "install", "-r", "requirements.txt"], 1),
+        (["mkdir", "-p", "~/bin"], 1),
         (["curl", "-Lo", "terraform.zip",  "https://releases.hashicorp.com/terraform/{0}/terraform_{0}_linux_amd64.zip".format(os.environ['TERRAFORM_VERSION'])], 1),
         (["mkdir", "-p", "~/.ssh"], 1),
-        (["unzip", "-d", "testing/cache/", "terraform.zip"], 1),
+        (["unzip", "-d", "~/bin/", "terraform.zip"], 1),
         (["ssh-keygen", "-N", '', "-f", "~/.ssh/id_rsa"], 1),
             ])
 
