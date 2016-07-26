598f140844fd0aa49338f352dc414dc777ccd9a7
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-07-26 12:23:21 -0500

    [TRAVIS] Symlink terraform file

--- testing/travis.py
+++ testing/travis.py
@@ -49,6 +49,7 @@ def script():
 
     # linter commands
     cmds = [
+        (["ln", "-s", os.environ["TERRAFORM_FILE"], "terraform.tf"], 1),
         (["terraform", "get"], 1),
         (["terraform", "plan", "--input=false", "--refresh=false"], 1),
     ]
